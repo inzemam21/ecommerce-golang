0fbeab44e82227593fab59be805022ab573e3d05
Inzimam Ul Haq <inzemamsmile2@gmail.com>
2024-08-06 21:48:56 +0400

    first commit

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	"github.com/go-sql-driver/mysql"
+	"github.com/inzemam21/ecommerce-golang/cmd/api"
+	"github.com/inzemam21/ecommerce-golang/configs"
+	"github.com/inzemam21/ecommerce-golang/db"
+)
+
+func main() {
+	db, err := db.NewMySQLStorage(mysql.Config{
+		User:                 configs.Envs.DBUser,
+		Passwd:               configs.Envs.DBPassword,
+		Addr:                 configs.Envs.DBAddress,
+		DBName:               configs.Envs.DBName,
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	initStorage(db)
+
+	server := api.NewApiServer(":8080", nil)
+	if err := server.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func initStorage(db *sql.DB) {
+	err := db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("DB: Successfully connected!")
+}
